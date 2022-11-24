526dcd42c0a4ffb69a4d25db137d1b22c44cf24b
Sven <sven.och@outlook.com>
2022-11-24 11:14:09 +0100

    new

--- Motor.py
+++ Motor.py
@@ -1,18 +1,18 @@
 from machine import Pin
 from time import sleep
 
-in1= Pin(19, Pin.OUT) 	#in1 coil 4
-in2= Pin(18, Pin.OUT)	#in2 coil 2
-in3= Pin(5, Pin.OUT) 	#in3 coil 3
-in4= Pin(17, Pin.OUT) 	#in4 coil 1
+in1= Pin(26, Pin.OUT)  #in1 coil 4
+in2= Pin(25, Pin.OUT)  #in2 coil 2
+in3= Pin(33, Pin.OUT)   #in3 coil 3
+in4= Pin(32, Pin.OUT)  #in4 coil 1
 
 
 pins = [in1, in2, in3, in4]
 
-sequenz = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]] # 
+sequenz = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]  
 
 while True:
     for step in sequenz:
         for i in range(len(pins)):
             pins[i].value(step[i])
-            sleep(0.1)
+            sleep(1)
---
+++ test.py
@@ -0,0 +1,15 @@
+from time import sleep
+in1 =1
+in2=2
+in3=3
+in4=4
+pins = [in4, in2, in3, in1]
+
+sequenz = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+while True:
+    for step in sequenz:
+        for i in
+            print(i)
+            #pins[i].value(step[i])
+            sleep(1)
