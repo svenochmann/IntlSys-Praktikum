efa54c679fbcab593a17f786586d97403270fcac
Sven <sven.och@outlook.com>
2022-12-15 12:28:31 +0100

    motor done

--- Motor.py
+++ Motor.py
@@ -1,18 +1,26 @@
 from machine import Pin
 from time import sleep
 
-in1= Pin(26, Pin.OUT)  #in1 coil 4
-in2= Pin(25, Pin.OUT)  #in2 coil 2
-in3= Pin(33, Pin.OUT)   #in3 coil 3
-in4= Pin(32, Pin.OUT)  #in4 coil 1
 
 
-pins = [in1, in2, in3, in4]
+in1= Pin(32, Pin.OUT)   #in1
+in2= Pin(33, Pin.OUT)   #in2  
+in3= Pin(25, Pin.OUT)   #in3
+in4= Pin(26, Pin.OUT)   #in4   
 
-sequenz = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]  
+pins = [in1,in2,in3,in4]
+seq =[[1,0,0,0],
+      [0,1,0,0],
+      [0,0,1,0],
+      [0,0,0,1]]
+
 
 while True:
-    for step in sequenz:
+    for step in seq:
         for i in range(len(pins)):
             pins[i].value(step[i])
-            sleep(1)
+            sleep(0.001)
+        
+        
+        
+        
--- motortest2.py
+++
@@ -1,8 +0,0 @@
-import stepper
-from machine import Pin
-
-s1=stepper.create(Pin(19,Pin.OUT),Pin(18,Pin.OUT),Pin(5,Pin.OUT),Pin(17,Pin.OUT), delay=2)
-s1.step(100)
-s1.step(100,-1)
-s1.angle(180)
-s1.angle(360,-1)
